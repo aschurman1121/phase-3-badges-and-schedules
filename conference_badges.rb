2497fa29230e9e0bc43cdf929407a423fa6e620a
alexandra <alexandraschurman@gmail.com>
2022-03-04 16:23:58 -0800

    commit tree 310010a45514686293ba9816522c95730cf4721e

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+
+def badge_maker(name)
+   return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    # method that takes an array of names as an argument and returns an array of badge messages.
+    array.map { |name| "Hello, my name is #{name}."}
+
+end
+
+def assign_rooms(speakers)
+    speakers.map.with_index do |person, index| 
+        "Hello #{person}! You'll be assigned to room #{index + 1 }!"
+    end
+end
